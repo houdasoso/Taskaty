7586e73de56646f1fd2b97dfa44c3071d0506b65
Houda <seddikihouda2304@example.com>
2025-07-30 23:44:34 +0100

    Add Phase 2: Authentication and Authorization

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
 
 app.use('/api/tasks', taskRoutes);
 
